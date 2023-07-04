3a1c3dc4ff304b99b2fe71b03f917809dedb8aa5
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-07-04 10:19:23 -0500

    Update aos.js

--- plugins/aos.js
+++ plugins/aos.js
@@ -2,6 +2,6 @@ import AOS from "aos";
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (typeof window !== "undefined") {
-    nuxtApp.AOS = AOS.init();
+    nuxtApp.AOS = AOS.init({ once: true });
   }
 });
