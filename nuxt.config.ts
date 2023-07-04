19e7fa410e5a449edc05b585245203afd3ec47fe
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-07-04 05:07:56 -0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,29 @@ export default ({
     "aos/dist/aos.css"
   ],
 
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      htmlAttrs: {
+        lang: "en"
+      },
+      meta: [
+        { name: "robots", content: "index, follow" }
+      ],
+      link : [
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "512x512", href: "/android-chrome-512x512.png" },
+        { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-chrome-192x192.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#181818" },
+      ]
+    }
+  },
+  
   modules: [
     "nuxt-icon"
   ],
-  
-  nitro: {
-    crawlLinks: true,
-    routes: ["/"]
-  }
 });
