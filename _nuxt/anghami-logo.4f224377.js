552b89d4a74b51716d3d58d7925cdf92ea596284
ahmedrangel <50090595+ahmedrangel@users.noreply.github.com>
2023-07-04 09:10:57 +0000

    Deploying to gh-pages from @ ahmedrangel/bayza@f4025f8683a2f1376d383effec2db128ff81e218 🚀

---
+++ _nuxt/anghami-logo.4f224377.js
@@ -0,0 +1,1 @@
+import"./entry.bcca29e0.js";const o=""+globalThis.__publicAssetsURL("images/anghami-logo.svg");export{o as _};
---
+++ _nuxt/config.5cbd6264.js
@@ -0,0 +1,1 @@
+import{N as p,O as o,u as i}from"./entry.bcca29e0.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
