931384f8beee28fb0cd49867f58d0bbbe580abf8
ahmedrangel <ahmedrangel@outlook.com>
2023-09-08 16:52:40 +0000

    Deploying to gh-pages from @ ahmedrangel/bayza@1fb11750d7ec7b863bb2852750e9599b170af0a8 🚀

---
+++ _nuxt/anghami-logo.1a346f6a.js
@@ -0,0 +1,1 @@
+import"./entry.ff2ca737.js";const o=""+globalThis.__publicAssetsURL("images/anghami-logo.svg");export{o as _};
---
+++ _nuxt/config.e9a1e7eb.js
@@ -0,0 +1,1 @@
+import{O as p,P as o,s as i}from"./entry.ff2ca737.js";const f={},s=p(f);function a(){const n=i();return n._appConfig||(n._appConfig=o(s)),n._appConfig}export{a as u};
