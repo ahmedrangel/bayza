bf80b71e456d50df6058c00f547c8c8a5b7d62bf
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-15 06:33:49 -0500

    chore: adjust transition

--- app.vue
+++ app.vue
@@ -9,12 +9,11 @@
 <style>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.1s ease;
+  transition: opacity 0.2s ease-in-out;
 }
 
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
-  filter: blur(0.3rem);
 }
 </style>
